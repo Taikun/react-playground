3b4de2004dbcda57ce9a5c71800aaeaf85775347
Daniel Lara <larapalma@gmail.com>
2023-05-09 07:28:50 +0000

    Button State

--- src/components/ButtonState.jsx
+++ src/components/ButtonState.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
+
 const ButtonState = () => {
-  let count = 0;
+  
+
+  const [count, setCount] = useState(1);
+
+  // const stateNumber = arrayState[0];
+  // const stateFuction = arrayState[1];
 
   const handleClick = () => {
-    count = count + 1;
-    console.log("handleClick: " + count);
+    setCount(count + 1);
   };
 
   return <button onClick={handleClick}>ButtonState: {count}</button>;
