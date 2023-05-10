2f97dc444bb5d8f76223a2877344d3abcda75fcd
Daniel Lara <larapalma@gmail.com>
2023-05-10 18:16:30 +0000

    code improvements

--- src/components/Controlado.jsx
+++ src/components/Controlado.jsx
@@ -2,13 +2,32 @@ import { useState } from "react";
 
 
 const Controlado = () => { 
-    const[todo, setTodo] = useState('')
-    const[descripcion, setDescripcion] = useState('')
-    const[state, setState] = useState('pendiente')
+
+    const [todo, setTodo] = useState({
+        title: "Todo #01",
+        descripcion: " Descripción #01",
+        state: "pendiente",
+        priority: false
+    });
+
+    const { title, descripcion, state, priority } = todo;
+
+    const handleChange = (e) => {
+        
+        const { name, value, type, checked } = e.target;
+
+        console.log(name);
+        if (type === "checkbox") {
+            console.log(checked);      
+        } else {
+            console.log(value);
+        }
+        setTodo({...todo, [name]: type === "checkbox" ? checked : value});
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-       
+       console.log(title, descripcion, state, priority);
     };
 
     return (
@@ -17,22 +36,28 @@ const Controlado = () => {
               type="text" 
               placeholder="Ingrese TODO" 
               className="form-control mb-2"
-              name = "todo" 
-              value={todo}
-              onChange={(e) => setTodo(e.target.value)}/>
+              name = "title" 
+              value={title}
+              onChange={handleChange}/>
             
+            <div className="form-check">
+              <input type="checkbox" name="priority" className="form-check-input mb-2" id="inputcheck" checked={priority} 
+             onChange={handleChange }/>
+              <label htmlFor="inputCheck">Dar Prioridad</label>
+            </div>
+
             <textarea
                 className="form-control mb2" 
                 placeholder="Ingrese Descripción"
                 name="descripcion"
                 value={descripcion}
-                onChange={(e) => setDescripcion(e.target.value)}>
+                onChange={handleChange}>
             </textarea>
             
             <select className="form-select mb-2" 
             name="state" 
             value={state} 
-            onChange={(e)=> setState(e.target.value)}>
+            onChange={handleChange}>
                 <option value="pendiente">Pendiente</option>
                 <option value="completado">Completado</option>
             </select>
